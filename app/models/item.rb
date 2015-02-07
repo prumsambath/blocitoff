1eff72a1d73401fb3ce95dd217287dd38c8a1ccf
Sambath Prum <psb.sambath@gmail.com>
2015-02-07 13:38:59 -0600

    add tests

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ActiveRecord::Base
   belongs_to :list
   after_create :set_to_incomplete
 
+  validates :name, presence: true
+
   def days_left
     7 - (DateTime.now.to_date - created_at.to_date).to_i
   end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,7 +2,18 @@ require 'rails_helper'
 
 describe Item do
   it { should validate_presence_of :name }
-  it { should belong_to :user}
+  it { should belong_to :list }
 
-  it "should be automatically removed after 7 days"
+  describe '#days_left' do
+    it 'should return the number of days remaining' do
+      item = create(:item)
+      expect(item.days_left).to eq(7)
+    end
+
+    it 'should return 5 when the item was created 2 days ago' do
+      item = create(:item, created_at: 2.days.ago)
+      expect(item.days_left).to eq(5)
+    end
+  end
+  xit "should be automatically removed after 7 days"
 end
